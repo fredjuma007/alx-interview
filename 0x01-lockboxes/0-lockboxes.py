6991e7a261ab0b9e3a9fc6f1d2a564727882e1a6
fredjuma007 <fredjuma007>
2023-07-04 22:15:33 +0200

    empty line i guess

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,8 +3,9 @@
 """
 Problem: You have n number of locked boxes in front of you.
          Each box is numbered sequentially from 0 to n - 1
+         -1 means an empty box 
          and each box may contain keys to the other boxes.
-Task: Write a method that determines if all the boxes can be opened.
+Task: Write a function that determins the probability of all boxes being opened.
 """
 
 
@@ -25,3 +26,4 @@ def canUnlockAll(boxes):
         if boxes_checked is False:
             return boxes_checked
     return True
+#Fred Juma
