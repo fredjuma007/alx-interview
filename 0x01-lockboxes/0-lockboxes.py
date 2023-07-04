98d60614d1d7ff8933cd067fe0b03a37e40c3f69
fredjuma007 <fredjuma007>
2023-07-04 21:50:29 +0200

    probability of opening boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,14 +2,14 @@
 
 
 def canUnlockAll(boxes):
-    """probability"""
+    """Unlock array of boxes of keys with indices"""
     size = len(boxes)
     checker = {}
     index = 0
 
     for keys in boxes:
         if len(keys) == 0 or index == 0:
-            checker[index] = -1  # -1 is an empty box
+            checker[index] = -1  # -1 means box is empty
         for key in keys:
                 if key < size and key != index:
                     checker[key] = key
