68e82d35c85b906e0de3f1fca7ed128f053efa4a
fredjuma007 <fredjuma007>
2023-07-11 17:00:11 +0200

    done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+calculates the fewest number of operarions needed
+to result exactly n H characters in the file
+
+AUTHER: Fred Juma
+"""
+
+
+def minOperations(n):
+    operation_count = 0
+    number = 2
+    if type(n) is not int or n <= 1:
+        return operation_count
+    while n > 1:
+        if n % number == 0:
+            operation_count += number
+            n /= number
+        else:
+            number += 1
+            return(operation_count)
