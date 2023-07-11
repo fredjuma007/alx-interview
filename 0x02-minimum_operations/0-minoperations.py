1038bd41faf941e679a7c7cdf36dc413e1e5bb61
fredjuma007 <fredjuma007>
2023-07-11 17:07:01 +0200

    correcting stuff

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,27 @@ to result exactly n H characters in the file
 
 AUTHER: Fred Juma
 """
+from sys import setrecursionlimit
 
 
 def minOperations(n):
-    operation_count = 0
-    number = 2
-    if type(n) is not int or n <= 1:
-        return operation_count
-    while n > 1:
-        if n % number == 0:
-            operation_count += number
-            n /= number
-        else:
-            number += 1
-            return(operation_count)
+
+    setrecursionlimit(10**6)
+
+    count = 0
+    inc = 2  # 3 4 5 6 etc.
+
+    # factor = 2
+    # no even division (prime) inc = n
+
+    if type(n) is not int or n < 2:
+        return 0
+
+    while n != 1:
+        if n % inc == 0:
+            count += inc  # 1
+            n /= inc
+            inc = 1  # resets for next loop
+        inc = inc + 1
+
+    return count
