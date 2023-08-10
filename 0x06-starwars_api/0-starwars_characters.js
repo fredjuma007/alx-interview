28dc26f92e98092eda5f8d1daa5971d2fa94ff28
fredjuma007 <fredjuma007>
2023-08-10 20:33:13 +0200

    MAY THE FORCE BE WITH YOU

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,28 @@
 #!/usr/bin/node
 
 const request = require('request');
+// May the for be with you
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
+                       //
+// Makes API request, sets async to allow await promise
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
 
-function getMovieCharacters(movieId) {
-    const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
+  // find URLs of each character in the film as a list obj
+  const charURLList = JSON.parse(body).characters;
 
-    request(movieUrl, (error, response, body) => {
-        if (error) {
-            console.error('Error:', error);
-            return;
-        }
+  // Use URL list to character pages to make new requests
+  // await queues requests until they resolve in order
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
 
-        if (response.statusCode !== 200) {
-            console.error('API Error:', response.statusCode, body);
-            return;
-        }
-
-        const movieData = JSON.parse(body);
-        const characterUrls = movieData.characters;
-        
-        characterUrls.forEach(characterUrl => {
-            request(characterUrl, (charError, charResponse, charBody) => {
-                if (charError) {
-                    console.error('Character Error:', charError);
-                    return;
-                }
-
-                const characterData = JSON.parse(charBody);
-                console.log(characterData.name);
-            });
-        });
+        // finds each character name and prints in URL order
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-}
-
-const movieId = process.argv[2];
-if (!movieId) {
-    console.log('Usage: node script.js <movieId>');
-    process.exit(1);
-}
-
-getMovieCharacters(movieId);
-
+  }
+});
